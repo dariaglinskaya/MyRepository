640d20e2016006f9fef89c67c3e64beddbc5bf3d
Daria Glinskaya <fpm.glinskaya@gmail.com>
2018-08-23 13:11:12 +0300

    Update login.js

--- UP/passport/login.js
+++ UP/passport/login.js
@@ -3,21 +3,21 @@ const userModel = require('../mongo/db').userModel;
 
 module.exports = (passport) => {
     passport.use('login', new LocalStrategy({
-                passReqToCallback: true
-            },
-            (req, username, password, done) => {
+     	passReqToCallback: true
+    },
+		(req, username, password, done) => {
             userModel.findOne({username: username}, (err, user) => {
-            if (err)
-            return done(err);
-    if (!user) {
-        console.log('User Not Found with username ' + username);
-        return done(null, false, {message: 'user not found.'});
-    }
-    if (password !== user.password) {
-        console.log('Invalid Password');
-        return done(null, false, {message: 'incorrect password.'});
-    }
-    done(null, user);
-            });
+            	if (err)
+            		return done(err);
+    			if (!user) {
+        			console.log('User Not Found with username ' + username);
+        			return done(null, false, {message: 'user not found.'});
+    			}
+    			if (password !== user.password) {
+        			console.log('Invalid Password');
+        			return done(null, false, {message: 'incorrect password.'});
+    			}
+    			done(null, user);
+           });
     }));
 };
